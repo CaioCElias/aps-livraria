ffd939bc67b0a929015b5035d195c6d16a8da583
Caio Elias <caiocesarelias1@gmail.com>
2023-10-25 20:45:45 -0300

    Padronizando nomenclatura de classes

---
+++ src/main/java/com/livraria/dao/ConnectionDAO.java
@@ -0,0 +1,50 @@
+package com.livraria.dao;
+
+import java.sql.*;
+
+import javax.swing.plaf.nimbus.State;
+
+// Classe para conectar com o banco de dados
+public class ConnectionDAO {
+    private static String DATABASE = "libraryw";
+    private static String URL = "jdbc:mysql://localhost:3306/" + DATABASE;
+    private static String USER = "root";
+    private static String PASS = "root";
+
+    private Connection dbconn = null;
+    private Statement sqlmgr = null;
+    private ResultSet resultsql = null;
+
+    public void openDatabase() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            dbconn = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("Conectado em " + URL);
+            sqlmgr = dbconn.createStatement();
+        } catch (Exception e) {
+            System.out.println("ERRO: " + e.getMessage());
+        }
+    }
+
+    public void closeDatabase() {
+        try {
+            if (sqlmgr != null) {
+                sqlmgr.close();
+            }
+            if (dbconn != null) {
+                dbconn.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+    }
+
+    public int ExecutaQuery(String sql) {
+        try {
+            return sqlmgr.executeUpdate(sql);
+        } catch (Exception e) {
+            System.out.println("ERRO: " + e.getMessage());
+        }
+        return -1;
+    }
+}
